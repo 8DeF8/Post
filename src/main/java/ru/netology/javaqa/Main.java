070e02c9e68de886ae2e48f43206f6f1b42d809f
DeF <DeF-2008@yandex.ru>
2023-10-22 18:49:03 +0300

    first commit

---
+++ src/main/java/ru/netology/javaqa/Main.java
@@ -0,0 +1,18 @@
+import ru.netology.javaqa.FormDate;
+import ru.netology.javaqa.Post;
+
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.name = "Иван";
+        post.pasport = "4444 № 44444444";
+        post.patromynic = "Иванович";
+        post.phone = "+7 (999)-999-99-99";
+        post.surname = "Иванов";
+        post.subscription = true;
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1999;
+    }
+}
---
+++ src/main/java/ru/netology/javaqa/Post.java
@@ -0,0 +1,11 @@
+package ru.netology.javaqa;
+
+public class Post {
+    public String name;
+    public String pasport;
+    public String patromynic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+    public FormDate birthday;
+}
